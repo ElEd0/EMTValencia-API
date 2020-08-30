cd2f000f8ecaf422a85493a1a382ae17e66ee903
ElEd0 <edoelgamer@gmail.com>
2020-08-30 18:52:15 +0200

    added setup and dist

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="emtvlc-api",
+	version="0.1",
+	author="Ed0",
+	author_email="ed0@ed0.com",
+	description="Python module to retrieve bus times and stops for EMT valencia",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/ElEd0/EMTValencia-API",
+	packages=setuptools.find_packages(),
+	install_requires=[
+		'requests'
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
